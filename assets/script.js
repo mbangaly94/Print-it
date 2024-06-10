07cbada12224d158319809f85a3b5aef6869cc08
SYLLABangaly <bangalysylla123@gmail.com>
2024-06-10 13:49:05 +0200

    First commit

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const slides = [
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+const bannerImg = document.querySelector(".banner-img");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot"); // Sélectionnez tous les points
+
+let currentIndex = 0;
